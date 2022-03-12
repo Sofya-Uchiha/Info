a78f752432a38e9cb1686cb2bf3a8f62484216d5
Mishina Sofya <93013927+Sofya-Uchiha@users.noreply.github.com>
2022-03-12 11:41:34 +0500

    Add files via upload

---
+++ t1/t1.js
@@ -0,0 +1,47 @@
+//function getResult(s){
+//
+//    return s[0].toUpperCase() + s.substring(1, s.lenght);
+//
+//}
+//
+//console.log(
+//    getResult('имя'),
+//    '==',
+//    'Имя'
+//);
+
+//function getResult(a){
+//
+//    var s = 0;
+//
+//    for(let i = 0; i < a.length; i++){
+//        s = s + a[i];
+//    }
+//
+//    return s
+//}
+//
+//console.log(getResult([1, 2, 3, 4, 5]), '==', 15)
+//console.log(getResult([-1, -2, -3, -4, -5]), '==', -15)
+//console.log(getResult([-2, -2, 0, 4]), '==', 0)
+
+//function getResult(s){
+//    var a = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+//
+//    for(let i =0; i < a.length; i++){
+//        s = s.replace(a[i], '!');
+//    }
+//
+//   return s;
+//
+//}
+//
+//console.log(
+//    getResult('Текст с числами 2 и 3 до !'),
+//    '==',
+//    'Текст с числами ! и ! до !'
+//)
+
+function getResult(){}
+
+console.log()
